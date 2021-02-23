8fa6f97dc68868c77fc0908cbc614d358aae9990
Jawad Rizvi <jawadrizvi40@gmail.com>
2021-02-22 17:14:03 -0700

    Lab 6 in progress

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,13 +9,12 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django-heroku
 import os
 from .secret_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -120,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals()) # bottom of the file
